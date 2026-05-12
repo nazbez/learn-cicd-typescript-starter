e14b7073afe8c7a8b35f68afffb17173defba89e
Nazar Bezuhlyi <vlm.9a.nazar.bezuglyi@gmail.com>
2026-05-12 19:39:33 +0300

    fix: update random bytes generation to use crypto.randomBytes for better security

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "eslint/config";
 import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
+  {ignores: ["dist/**", "node_modules/**"]},
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
